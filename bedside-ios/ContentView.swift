55893694d86b0bfaca892414b2258554ab8a3c9f
Andy Lenox <alenox@umich.edu>
2020-02-12 09:20:53 -0500

    Initial Commit

---
+++ bedside-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bedside-ios
+//
+//  Created by Andrew Lenox on 2/12/20.
+//  Copyright © 2020 Andrew Lenox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bedside-iosTests/bedside_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  bedside_iosTests.swift
+//  bedside-iosTests
+//
+//  Created by Andrew Lenox on 2/12/20.
+//  Copyright © 2020 Andrew Lenox. All rights reserved.
+//
+
+import XCTest
+@testable import bedside_ios
+
+class bedside_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
