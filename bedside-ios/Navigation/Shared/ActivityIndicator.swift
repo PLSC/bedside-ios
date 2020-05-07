a2f0a665250140d54e7a2b5c7bc073183b11af51
Andy Lenox <alenox@umich.edu>
2020-05-07 12:14:33 -0400

    add activity indicator.

---
+++ bedside-ios/Navigation/Shared/ActivityIndicator.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityIndicator.swift
+//  bedside-ios
+//
+//  Created by Andrew Lenox on 5/7/20.
+//  Copyright © 2020 Andrew Lenox. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
