5e582969e8be8f853dddd7b2731b04c02d80eece
Andy Lenox <alenox@umich.edu>
2020-06-08 09:34:03 -0400

    hotfix for password requirements

--- bedside-ios/Login/ForgotPasswordView.swift
+++ bedside-ios/Login/ForgotPasswordView.swift
@@ -26,7 +26,7 @@ class ForgotPasswordViewModel : ObservableObject {
     
     var passwordIsValid : AnyPublisher<Bool, Never> {
         $password.map {
-            $0.count > 5
+            $0.count > 7
         }.eraseToAnyPublisher()
     }
     
